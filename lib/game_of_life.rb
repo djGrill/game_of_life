8de9685ff096587dcebd746dc7d6e7a00694bc80
David Grilli <djgrill@gmail.com>
2013-07-19 21:24:13 +0100

    Adds some implementations to Cell class and some test definitions

--- lib/game_of_life.rb
+++ lib/game_of_life.rb
@@ -40,35 +40,51 @@ module GameOfLife
       def next_generation
       end
 
+
       class Cell
-          def initialize
+          attr_accessor :row, :col, :status, :alive_neighboors, :matrix
+
+          def initialize(row, col, status, matrix)
+              @row = row
+              @col = col
+              @status = status
+              @matrix = matrix
+              @alive_neighboors = self.count_alive_neighboors
           end
 
           def count_alive_neighboors
           end
 
           def is_alive?
+              return @status == "x"
           end
 
           def is_underpopulated?
+              return @alive_neighboors < 2
           end
 
           def is_overpopulated?
+              return @alive_neighboors > 3
           end
 
           def is_reproductive?
+              return @alive_neighboors == 3
           end
 
           def must_live?
+              return (!self.is_alive? and self.is_reproductive?)
           end
 
           def must_die?
+              return (self.is_alive? and (self.is_underpopulated? or self.is_overpopulated?))
           end
 
           def revive
+              @status = "x"
           end
 
           def kill
+              @status = " "
           end
       end
   end
--- spec/lib/game_of_life_spec.rb
+++ spec/lib/game_of_life_spec.rb
@@ -57,15 +57,139 @@ describe GameOfLife::Game do
 
   it 'should run game for one round' do
     @game.run
-    @game.board == @after_one_board
+    @game.board.should == @after_one_board
   end
 
   it 'should run game for five round' do
     @game.run(5)
-    @game.board == @after_five_board
+    @game.board.should == @after_five_board
   end
 
   it 'should print board' do
     @game.board.to_s.should == @starting_board
+  end
+end
+
+
+describe GameOfLife::Board do
+  before(:all) do
+    @starting_board  = "   xxx   \n"
+    @starting_board += "         \n"
+  end
+
+  before(:each) do
+    @board = GameOfLife::Board.new(@starting_board)
+  end
+
+  it 'should be initialized without arguments' do
+    lambda { GameOfLife::Board.new }.should_not raise_error
+  end
+
+  it 'should be initialized with arguments' do
+    lambda { GameOfLife::Board.new(@starting_board) }.should_not raise_error
+  end
+
+  it 'should respond to matrix' do
+    @board.should respond_to(:matrix)
+  end
+
+  it 'should respond to to_s' do
+    @board.should respond_to(:to_s)
+  end
+
+  it 'should respond to next_generation' do
+    @board.should respond_to(:next_generation)
+  end
+
+  it '#to_s should return starting board after initializazion' do
+    @board.to_s.should == @starting_board
+  end
+
+  it 'should print board' do
+    @board.to_s.should == @starting_board
+  end
+
+
+  describe GameOfLife::Board::Cell do
+    before(:each) do
+      @board = GameOfLife::Board.new(@starting_board)
+      @matrix = @board.matrix
+      @first_cell = GameOfLife::Board::Cell.new(0, 0, @matrix[0][0], @matrix)
+    end
+
+    it 'should respond to count_alive_neighboors' do
+      @first_cell.should respond_to(:count_alive_neighboors)
+    end
+
+    it 'should respond to is_alive?' do
+      @first_cell.should respond_to(:is_alive?)
+    end
+
+    it 'should respond to is_underpopulated?' do
+      @first_cell.should respond_to(:is_underpopulated?)
+    end
+
+    it 'should respond to is_overpopulated?' do
+      @first_cell.should respond_to(:is_overpopulated?)
+    end
+
+    it 'should respond to is_reproductive?' do
+      @first_cell.should respond_to(:is_reproductive?)
+    end
+
+    it 'should respond to must_die?' do
+      @first_cell.should respond_to(:must_die?)
+    end
+
+    it 'should respond to must_live?' do
+      @first_cell.should respond_to(:must_live?)
+    end
+
+    it 'should respond to kill' do
+      @first_cell.should respond_to(:kill)
+    end
+
+    it 'should respond to revive' do
+      @first_cell.should respond_to(:revive)
+    end
+
+    it '#count_alive_neighboors should count 0 neighboors' do
+    end
+
+    it '#count_alive_neighboors should count 1 neighboor' do
+    end
+
+    it '#count_alive_neighboors should count 2 neighboors' do
+    end
+
+    it '#count_alive_neighboors should count 3 neighboors' do
+    end
+
+    it '#count_alive_neighboors should count 4 neighboors' do
+    end
+
+    it '#is_alive? should detect living and dead cells' do
+    end
+
+    it '#is_underpopulated? should detect underpopulated cells' do
+    end
+
+    it '#is_overpopulated? should detect overpopulated cells' do
+    end
+
+    it '#is_reproductive? should detect reproductive cells' do
+    end
+
+    it '#must_die? should detect cells that must die' do
+    end
+
+    it '#must_live? should detect cells that must live' do
+    end
+
+    it '#revive should revive cells' do
+    end
+
+    it '#kill should kill cells' do
+    end
   end
 end
