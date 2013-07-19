921fbecb32557311a4f463b23a89a3eb70c54974
David Grilli <djgrill@gmail.com>
2013-07-19 20:47:11 +0100

    Makes existing tests pass

--- lib/game_of_life.rb
+++ lib/game_of_life.rb
@@ -4,5 +4,19 @@
 #
 module GameOfLife
   class Game
+      attr_accessor :board
+
+      def initialize(starting_board = nil)
+          if not starting_board.nil?
+              @board = starting_board
+          end
+      end
+
+      def board
+          @board
+      end
+
+      def run(generations = 1)
+      end
   end
 end
